498e236b7ace94d144e50fbcef4272fa673e3d48
root <brandonhinds13@gmail.com>
2016-07-19 02:28:03 -0400

    Heruko Static Fix 3

--- membership/settings.py
+++ membership/settings.py
@@ -106,5 +106,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
