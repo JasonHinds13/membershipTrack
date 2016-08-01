44e66b139b74e7171983047c991d79b83f3b0ca1
root <brandonhinds13@gmail.com>
2016-07-31 21:44:53 -0400

    upload for pics changed fixed again

--- membership/settings.py
+++ membership/settings.py
@@ -103,6 +103,8 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
