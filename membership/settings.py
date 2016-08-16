89e0f9b75e83f01f79e08afad3ff84702afd12ad
root <brandonhinds13@gmail.com>
2016-08-15 21:20:56 -0400

    Removed Heroku Issue

--- membership/settings.py
+++ membership/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '19@ww7h9(07gxci^4$w1a04@#smn*bv%4#-njx7*4tk!@b589p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
