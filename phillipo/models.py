62b3478e46610f184f78f554d19206725cec6482
root <brandonhinds13@gmail.com>
2016-08-16 20:52:50 -0400

    Removed defaults for member model fields

--- phillipo/models.py
+++ phillipo/models.py
@@ -20,16 +20,16 @@ MONTHS = (
 class Member(models.Model):
     photo = models.ImageField(upload_to='images/', max_length=255)
 
-    name = models.CharField(max_length=255, default='000')
-    address = models.CharField(max_length=255, default='000')
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
 
-    birth_month = models.CharField(max_length=255, default='000', choices=MONTHS)
-    birth_day = models.CharField(max_length=255, default='000', choices=[(str(x), str(x)) for x in range(1, 32)])
-    birth_year = models.CharField(max_length=255, default='000')
+    birth_month = models.CharField(max_length=255, choices=MONTHS)
+    birth_day = models.CharField(max_length=255, choices=[(str(x), str(x)) for x in range(1, 32)])
+    birth_year = models.CharField(max_length=255)
 
-    profession = models.CharField(max_length=255, default='000')
-    ministry = models.CharField(max_length=255, default='000')
-    classNumber = models.CharField(max_length=255, default='000')
+    profession = models.CharField(max_length=255)
+    ministry = models.CharField(max_length=255)
+    classNumber = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
